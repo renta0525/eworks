c958d6359ff2f71310ec61e8ee454f136b12bd3f
renta0525 <renzu525@gmail.com>
2024-07-24 03:07:00 +0900

    user情報の編集ができなくなった（後で確認）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update(user_params)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
       redirect_to root_path
     else
       render :edit, status: :unprocessable_entity
